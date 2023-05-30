2116de8acd47bb51e4ee3b405ed6b402e241be76
bim121 <vitisik54@gmail.com>
2023-05-30 22:03:43 +0300

    fix joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import Joi from 'joi';
+import * as Joi from '@hapi/joi'
 
 @Module({
   imports: [
@@ -20,16 +20,6 @@ import Joi from 'joi';
         AWS_ACCESS_KEY_ID: Joi.string().required(),
         AWS_SECRET_ACCESS_KEY: Joi.string().required(),
         AWS_PUBLIC_BUCKET_NAME: Joi.string().required(),
-        WEBHOOK_URL: Joi.string().required(),
-        JWT_SECRET: Joi.string().required(),
-        JWT_EXPIRATION_TIME: Joi.string().required(),
-        RABBITMQ_USER: Joi.string().required(),
-        RABBITMQ_PASSWORD: Joi.string().required(),
-        RABBITMQ_HOST: Joi.string().required(),
-        RABBITMQ_QUEUE_NAME: Joi.string().required(),
-        ELASTICSEARCH_NODE: Joi.string().required(),
-        ELASTICSEARCH_USERNAME: Joi.string().required(),
-        ELASTICSEARCH_PASSWORD: Joi.string().required(),
       })
     }),
   ],
